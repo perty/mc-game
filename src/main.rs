d02a775872066376e756001bc84fa6e59905a698
Per Lundholm <per@artcomputer.se>
2023-07-07 10:56:31 +0200

    Chapter 3.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use macroquad::prelude::*;
 
+const MOVEMENT_SPEED: f32 = 200.0;
+const RADIUS: f32 = 16.0;
+
 #[macroquad::main("McGame")]
 async fn main() {
 
@@ -7,22 +10,26 @@ async fn main() {
     let mut y = screen_height() / 2.0;
 
     loop {
+        let delta_time = get_frame_time();
+
         clear_background(DARKPURPLE);
 
         if is_key_down(KeyCode::Right) {
-            x += 1.0;
+            x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= 1.0;
+            x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += 1.0;
+            y += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= 1.0;
+            y -= MOVEMENT_SPEED * delta_time;
         }
+        x = x.min(screen_width() - RADIUS).max(RADIUS);
+        y = y.min(screen_height() - RADIUS).max(RADIUS);
 
-        draw_circle(x, y, 16.0, YELLOW);
+        draw_circle(x, y, RADIUS, YELLOW);
 
         next_frame().await
     }
