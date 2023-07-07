9b01bfd06cb7c19a25bc3a98b376357fca3da2f8
Per Lundholm <per@artcomputer.se>
2023-07-07 11:16:38 +0200

    Chapter 4. Falling rectangles.

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,71 @@ use macroquad::prelude::*;
 const MOVEMENT_SPEED: f32 = 200.0;
 const RADIUS: f32 = 16.0;
 
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
+
 #[macroquad::main("McGame")]
 async fn main() {
-
-    let mut x = screen_width() / 2.0;
-    let mut y = screen_height() / 2.0;
-
+    rand::srand(miniquad::date::now() as u64);
+    let mut squares = vec![];
+    let mut circle = Shape {
+        size: 32.0,
+        speed: MOVEMENT_SPEED,
+        x: screen_width() / 2.0,
+        y: screen_height() / 2.0,
+    };
     loop {
         let delta_time = get_frame_time();
 
         clear_background(DARKPURPLE);
 
         if is_key_down(KeyCode::Right) {
-            x += MOVEMENT_SPEED * delta_time;
+            circle.x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= MOVEMENT_SPEED * delta_time;
+            circle.x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += MOVEMENT_SPEED * delta_time;
+            circle.y += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= MOVEMENT_SPEED * delta_time;
+            circle.y -= MOVEMENT_SPEED * delta_time;
         }
-        x = x.min(screen_width() - RADIUS).max(RADIUS);
-        y = y.min(screen_height() - RADIUS).max(RADIUS);
+        circle.x = circle.x.min(screen_width() - RADIUS).max(RADIUS);
+        circle.y = circle.y.min(screen_height() - RADIUS).max(RADIUS);
 
-        draw_circle(x, y, RADIUS, YELLOW);
+        // Generate a new square maybe
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                y: -size,
+            });
+        }
+
+        // Move squares
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+        // Remove squares below bottom of screen
+        squares.retain(|square| square.y < screen_width() + square.size);
+
+        draw_circle(circle.x, circle.y, RADIUS, YELLOW);
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size,
+                GREEN,
+            );
+        }
 
         next_frame().await
     }
